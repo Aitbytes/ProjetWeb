054bbb887485b5a37e2b32803f295e20ca2744a2
Sakji Hilmi <sakji@insa-20714>
2022-11-30 14:44:24 +0100

    o

---
+++ function.js
@@ -0,0 +1,4 @@
+const DEALER = document.getElementById("dealer") ; 
+const PLAYER = document.getElementById("player") ; 
+const Hit_BUTTON = document.getElementById("hit_button") ; 
+const Pass_BUTTON = document.getElementById("pass_button") ; 

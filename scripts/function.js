d953bd24348394879ff115ee2fab6e1fd44d7014
invochex <119500659+invochex@users.noreply.github.com>
2022-12-12 16:01:30 +0100

    Update function.js

--- scripts/function.js
+++ scripts/function.js
@@ -1,5 +1,13 @@
-
 const DEALER = document.getElementById("dealer") ; 
 const PLAYER = document.getElementById("player") ; 
 const Hit_BUTTON = document.getElementById("hit_button") ; 
 const Pass_BUTTON = document.getElementById("pass_button") ; 
+
+ 
+const dealHand = () => {
+    const hand=[pullRandomCard(), pullRandomCard()]
+    hand[0].shown= (true)
+    
+    console.log(hand)
+    
+}
